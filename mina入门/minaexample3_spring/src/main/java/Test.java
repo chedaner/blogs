9bead7f53da45b4d78589fa3216d792bbab0b7a7
chedanerniubi <chedanerniubi@163.com>
2019-04-09 23:20:20 +0800

    mina_starter

---
+++ mina入门/minaexample3_spring/src/main/java/Test.java
@@ -0,0 +1,12 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class Test {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext ct =    new ClassPathXmlApplicationContext("applicationContext-mina.xml");
+    }
+
+}
